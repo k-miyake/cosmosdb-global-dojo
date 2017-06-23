6ed6318989b3ede7781ef1386fe8139a52790e15
Kazuyuki Miyake <miyake@zenarc.jp>
2017-06-23 23:07:59 +0900

    init

---
+++ src/DocumentDBRepository.cs
@@ -0,0 +1,160 @@
+﻿namespace todo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Documents;
+    using Microsoft.Azure.Documents.Client;
+    using Microsoft.Azure.Documents.Linq;
+
+    public static class DocumentDBRepository<T> where T : class
+    {
+        private static readonly string DatabaseId = ConfigurationManager.AppSettings["database"];
+        private static readonly string CollectionId = ConfigurationManager.AppSettings["collection"];
+        private static DocumentClient client;
+
+        public static async Task<T> GetItemAsync(string id)
+        {
+            try
+            {
+                Document document = await client.ReadDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+                return (T)(dynamic)document;
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        public static async Task<IEnumerable<T>> GetItemsAsync(Expression<Func<T, bool>> predicate)
+        {
+            IDocumentQuery<T> query = client.CreateDocumentQuery<T>(
+                UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId),
+                new FeedOptions { MaxItemCount = -1 })
+                .Where(predicate)
+                .AsDocumentQuery();
+
+            List<T> results = new List<T>();
+            while (query.HasMoreResults)
+            {
+                results.AddRange(await query.ExecuteNextAsync<T>());
+            }
+
+            return results;
+        }
+
+        public static async Task<Document> CreateItemAsync(T item)
+        {
+            return await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), item);
+        }
+
+        public static async Task<Document> UpdateItemAsync(string id, T item)
+        {
+            return await client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id), item);
+        }
+
+        public static async Task DeleteItemAsync(string id)
+        {
+            await client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id));
+        }
+
+        public static string GetReadEndpoint()
+        {
+            var ep = client.ReadEndpoint.Host;
+            return ep;
+        }
+
+        public static string GetWriteEndpoint()
+        {
+            var ep = client.WriteEndpoint.Host;
+            return ep;
+        }
+
+        public static void Initialize()
+        {
+            // 読み取りエンドポイント優先順位
+            ConnectionPolicy cp = new ConnectionPolicy();
+
+            switch (ConfigurationManager.AppSettings["appRegion"])
+            {
+                case "japanwest":
+                    cp.PreferredLocations.Add(LocationNames.JapanWest);
+                    cp.PreferredLocations.Add(LocationNames.JapanEast);
+                    cp.PreferredLocations.Add(LocationNames.EastUS2);
+                    break;
+                case "eastus2":
+                    cp.PreferredLocations.Add(LocationNames.EastUS2);
+                    cp.PreferredLocations.Add(LocationNames.JapanWest);
+                    cp.PreferredLocations.Add(LocationNames.JapanEast);
+                    break;
+            }
+
+            cp.EnableEndpointDiscovery = true;
+
+            // 書き込みエンドポイント
+            client = new DocumentClient(
+                new Uri(ConfigurationManager.AppSettings["endpoint"]), // エンドポイント
+                ConfigurationManager.AppSettings["authKey"],
+                cp
+            );
+
+            client.OpenAsync(); // パフォーマンス改善のため一度接続しておく
+
+            CreateDatabaseIfNotExistsAsync().Wait();
+            CreateCollectionIfNotExistsAsync().Wait();
+
+            
+        }
+
+        private static async Task CreateDatabaseIfNotExistsAsync()
+        {
+            try
+            {
+                await client.ReadDatabaseAsync(UriFactory.CreateDatabaseUri(DatabaseId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await client.CreateDatabaseAsync(new Database { Id = DatabaseId });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private static async Task CreateCollectionIfNotExistsAsync()
+        {
+            try
+            {
+                await client.ReadDocumentCollectionAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId));
+            }
+            catch (DocumentClientException e)
+            {
+                if (e.StatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    await client.CreateDocumentCollectionAsync(
+                        UriFactory.CreateDatabaseUri(DatabaseId),
+                        new DocumentCollection { Id = CollectionId },
+                        new RequestOptions { OfferThroughput = 1000 });
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
